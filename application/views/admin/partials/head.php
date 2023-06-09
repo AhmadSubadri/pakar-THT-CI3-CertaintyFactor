d0cc49146e2698221322a4eeb177ce1aba7b3772
Ahmad Subadri <ahmadsubadri953@gmail.com>
2023-06-09 13:34:13 +0700

    title

--- application/views/admin/partials/head.php
+++ application/views/admin/partials/head.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistem Pakar Pinilih - </title>
+    <title>Sispak | THT, Sistem pakar Diagnosis  </title>
     <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url() ?>assets/asset/img/favicons/icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url() ?>assets/asset/img/favicons/icon.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>assets/asset/img/favicons/icon.png">
